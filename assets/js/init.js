0bc45deeb95509e59bc4d0c65e1393ec4d8b1554
Mostafa Amiri <m.amiri@samansystems.com>
2017-08-29 16:29:51 +0430

    Add missing static files
    This reverts commit 84f31a9b70273d3615044106d3e835815cb30e8f.

---
+++ assets/js/init.js
@@ -0,0 +1,2 @@
+"use strict";requirejs.config({baseUrl:"/assets/js/",paths:{jquery:"lib/jquery",slick:"lib/slick/slick.min",TweenMax:"lib/TweenMax",app:"initial"}});
+//# sourceMappingURL=init.js.map
