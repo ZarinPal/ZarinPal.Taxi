46a34664684cbc288db2a2b30c5a993545ed1b2f
RTLer <hooman@rtler.com>
2017-08-29 17:43:54 +0430

    Fix compiler.sh assets copy

--- compiler.sh
+++ compiler.sh
@@ -10,7 +10,7 @@ chmod 600 .deploy_key
 ssh-add .deploy_key
 git clone -b gh-pages git@github.com:ZarinPal/ZarinPal.taxi.git public
 npm start production
-cp -r assets public/assets
+cp -a assets/. public/assets/
 cp index.html public/index.html
 cd public
 git add --all .
